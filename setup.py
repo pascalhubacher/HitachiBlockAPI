d1ee146f2ffc38aec31b06b99281425e12223e0d
pascalhubacher <pascal.hubacher@outlook.com>
2020-12-07 09:20:14 +0100

    version changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='StorageRestAPI',
     packages=['StorageRestAPI'],
-    version='0.0.7',
+    version='0.9.8',
     description='Hitachi Storage REST API Project',
     url='https://github.com/pascalhubacher/HitachiStorageRestAPI',    
 )
