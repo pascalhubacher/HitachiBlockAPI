f9c10460af183d28a2d53f1cbaeceb0de91ae69c
Christian Hässler <christian.haessler@coop.ch>
2020-12-09 16:11:09 +0100

    Cleanup of setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,27 @@
 import setuptools
 
-#README.md auslesen und als description verwenden
+# Read infos from README.md
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# zwingende Abhaengigkeiten
-# Hier einfach den Inhalt vom requirements einfuegen.
-# wohl ohne das Ganze Anaconda zeuggs am Schluss.
-# Ich wuerde auch ohne fixe Versions angabe.
+# mandatory depencies
 deps = []
 
-
-# Optionale Abhaengigkeiten
-# Installieren mit pip install MODULENAME['full']
-# Hier koennte man auch die moeglichkeit bieten, eine Version zu machen
-# fuer entwickler und eine fuer user. Also full mit allen Test Modulen
-# und eine User Version mit nur dem noetigsten zum starten...
+# optional depencies for "maintainer"
 optional_deps = {
         "full": ['wheel', 'setuptools', 'pytest', 'pytest-html', 'keyring', 'twine'],
         "minimal": []
         }
 
-
 setuptools.setup(
     name='HitachiBlockAPI',
     packages=['HitachiBlockAPI'],
-    version='0.9.2',
+    version='0.9.3',
     author="Pascal Hubacher",
-    description='Hitachi Storage REST API to ease the communication to Hitachi Storage',
+    description='Python Class for Hitachi Storage REST API to ease the communication to Hitachi Storage',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/pascalhubacher/HitachiStorageRestAPI',
+    url='https://github.com/pascalhubacher/HitachiBlockAPI',
     install_requires=deps,
     extras_require=optional_deps,
     scripts=[]
